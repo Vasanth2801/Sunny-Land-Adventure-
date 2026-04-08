10cc51ba32740542fa3456cb9a7332d12683f619
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-08 09:31:17 +0530

    Player Move and Jump

---
+++ Sunny Land Adventure/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 13f;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Inputs")]
+    [SerializeField] private float moveInput;
+
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
