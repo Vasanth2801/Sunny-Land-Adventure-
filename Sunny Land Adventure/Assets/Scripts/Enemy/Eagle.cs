6629dbc0543817b971f967bebdf852f7fc3860f6
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-08 13:27:13 +0530

    BAt and Eagle Enemy with Animations

---
+++ Sunny Land Adventure/Assets/Scripts/Enemy/Eagle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Eagle : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 1f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector2(speed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        speed = -speed;
+        FlipFacingEnemy();
+    }
+
+    void FlipFacingEnemy()
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(rb.linearVelocity.x),1f);
+    }
+}
